e4d342537124618972a14fed8431acf75de61d11
tusherbhakta <tusheranta@gmail.com>
2025-09-08 11:44:06 +0530

    Refactor background image import in App component for improved path handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Dashboard from "./pages/Dashboard";
+import bgImage from "./assets/bgImage.png";
 
 function App() {
   return (
     <div
       className="min-h-screen font-poppins bg-cover bg-center "
-      style={{ backgroundImage: "url('/src/assets/bgImage.png')" }}
+      style={{ backgroundImage: `url(${bgImage})` }}
     >
       <Dashboard />
     </div>
